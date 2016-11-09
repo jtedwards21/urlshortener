4df96d9a9354b78f198268a497fc8011d25c8e3b
James Ahlm <14782828428@qq.com>
2016-11-09 15:17:55 +0800

    Can get create new entries in db

--- server.js
+++ server.js
@@ -7,19 +7,20 @@ var db = mongojs("urldb", ["urls"]);
 
 app.get('/new/:long', function (req, res) {
   //Get the last short url from the db
-  var cursor = db.urldb.find().sort({short: -1});
-  console.log(cursor);
-  var short = 4
+　　var short
+  var cursor = db.urls.find(function (err, docs){
+	docs.sort({short: -1});　// This is not actually sorting because it's an array function, not mongo
+　　　　　　　　short = docs[0].short　+ 1;
+	//Insert the short and long urls into the db
+  	db.urls.insert({longurl: req.params.long, short: short});
 
-  //Insert the short and long urls into the db
-  db.urls.insert({longurl: req.params.long, short: short});
-
-  //display the information in JSON format
-  res.send(JSON.stringify({long: req.params.long, short: short}));
-　　
+  	//display the information in JSON format
+  	res.send(JSON.stringify({long: req.params.long, short: short}));
+  });
 });
 
 app.get('/:short', function (req, res) {
+　　console.log('short');
   //Get the corresponding long url from the db
   var cursor = db.urldb.find({short: req.params.short});
   console.log(cursor);
